b66a878a9dd14701c36a630e3ef45507a5a3a2f1
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2022-05-20 15:43:31 +0200

    ➕ added cors + removed pug

---
+++ api/app.js
@@ -0,0 +1,31 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  console.error(err);
+});
+
+module.exports = app;
