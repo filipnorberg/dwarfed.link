f6e163ac2c3743ccd992e0435f3fde089e50a5a3
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2022-05-20 15:44:46 +0200

    ✨ created frontend able to send request to api

---
+++ web/js/script.js
@@ -0,0 +1,17 @@
+const formElement = document.getElementById('linkForm');
+const inputElement = document.getElementById('linkInput');
+
+formElement.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const data = { orgLink: inputElement.value }
+    const response = await fetch("http://localhost:3000/links", {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(data)
+    });
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+})
