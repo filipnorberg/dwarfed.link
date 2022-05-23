a189fcb61fd0005ced49fb9e64a49947731806df
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2022-05-23 15:56:09 +0200

    🔥 removed old console.logs

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,6 @@ const db = require('../db');
 
 
 router.get('/:id', async function(req, res, next) {
-  console.log(req.params.id);
   const orgLink = await db.getOriginalLink(req.params.id);
   return res.redirect(orgLink);
 });
