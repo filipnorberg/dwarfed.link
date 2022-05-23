79a41810cad31fa796f6d15a90192966bd8ced99
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2022-05-23 14:31:49 +0200

    ✨ new links now create new database objects

--- api/db.js
+++ api/db.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require("mongodb");
+const Chance = require('chance');
 
 require('dotenv').config();
 
 
 const client = new MongoClient(process.env.MONGODB_URL);
+
+const chance = new Chance();
 
 
 async function getShortLinkID(orgLink) {
@@ -13,11 +16,21 @@ async function getShortLinkID(orgLink) {
     const links = database.collection('links');
     const query = { orgLink: orgLink };
     const link = await links.findOne(query);
-    return link.shortLinkID;
+
+    if (link == null) {
+      const newShortID = chance.string({ length: 8, pool: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"});
+      await links.insertOne(
+        { orgLink: orgLink, shortLinkID: newShortID }
+      );
+      return newShortID;
+    } else {
+      return link.shortLinkID;
+    }
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
 
 module.exports = { getShortLinkID }
